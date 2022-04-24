5ad96e9edbfeb714d80867bed28a9a366ff0cff7
usableusername <62962225+usableusername@users.noreply.github.com>
2022-04-24 19:03:03 +0300

    Update BULBASAUR NO HUMANS.py

--- BULBASAUR NO HUMANS.py
+++ BULBASAUR NO HUMANS.py
@@ -1,117 +1,89 @@
+import matplotlib.pyplot as plt
 import random
-import math
-import csv
 
+plt.xlabel("ticks")
+plt.ylabel("population")
+plt.title("1 Female to 7 Males")
+def sim(male, female):
+  #"Rendering"
+  MalePop = male*2
+  FemalePop = female*2
+  tick = 0
+  eggs = [0]*21
+  
+  
+  x=[]
+  yAll = []
+  yMales = []
+  yFemales = []
+  
+  while tick < 300:
+      x.append(tick)
+      yAll.append(MalePop+FemalePop)
+      yMales.append(MalePop)
+      yFemales.append(FemalePop)
+  
+      tick += 1
+      print(f"tick: {tick}\n{yAll[-1]}")
+      #The Death Chamber TM
+      for i in range(MalePop):
+          isDeath = random.randint(1, 10000) <= 243
+          if isDeath:
+              MalePop -= 1
+      for i in range(FemalePop):
+          isDeath = random.randint(1, 10000) <= 243
+          if isDeath:
+              FemalePop -= 1
+      if eggs[0] > 0:
+          for i in range(eggs[0]):
+              genderroll = random.randint(1, female+male)
+              if genderroll >= female:
+                  FemalePop += 1
+              else:
+                  MalePop += 1
+      eggs.pop(0)
+      eggs.append(0)
+  
+      #Reproduction
+      if MalePop>=1:
+          for i in range(FemalePop):
+              isEgg = random.randint(1, 10) <= 5
+              if isEgg:
+                  eggs[-1] += 1
+      elif FemalePop>=1:
+          for i in range(FemalePop):
+              isEgg = random.randint(1, 10) <= 1
+              if isEgg:
+                  eggs[-1] += 1
+      
+      #exit point
+      if FemalePop+MalePop + sum(eggs) == 0:
+          print("Extinction")
+          break
+  
+  print(f"all: {FemalePop+MalePop}\nmale: {MalePop}\nfemale: {FemalePop}\nRatio: {male}:{female}")
+  return yAll
+x = []
+for i in range(1,301):
+  x.append(i) 
+eighthFemales = sim(7, 1)
 
-MalePop = 14
-FemalePop = 2
-tick = 0
-Egg0 = 0
-Egg1 = 0
-Egg2 = 0
-Egg3 = 0
-Egg4 = 0
-Egg5 = 0
-Egg6 = 0
-Egg7 = 0
-Egg8 = 0
-Egg9 = 0
-Egg10 = 0
-Egg11 = 0
-Egg12 = 0
-Egg13 = 0
-Egg14 = 0
-Egg15 = 0
-Egg16 = 0
-Egg17 = 0
-Egg18 = 0
-Egg19 = 0
-Egg20 = 0
+forthFemales = sim(6, 2)
 
-with open('BulbasaurPopulationNoHuman.csv', 'a', newline='') as csv_file:
-        fieldnames = ['tick', 'females', 'males']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        csv_file.close()
+halfHalf = sim(4, 4)
 
-while True:
-    with open('BulbasaurPopulationNoHuman.csv', 'a', newline='') as csv_file:
-        fieldnames = ['tick', 'females', 'males']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writerow({'tick': tick, 'females': FemalePop, 'males': MalePop})
-        csv_file.close()
-    tick += 1
-    print("Tick:", tick)
-    for x in range(FemalePop):
-        death = random.randint(1,10000)
-        if death <= 243:
-            FemalePop += -1
-            
+forthMales =sim(2, 6)
 
-    for x in range(MalePop):
-        death = random.randint(1,10000)
-        if death <= 243:
-            MalePop += -1
-            
-    
-    if Egg0 > 0:
-        print (Egg0, "Bulbasaur eggs are hatching!")
-        for x in range(Egg0):
-            sexroll = random.randint(1,8)
-            if sexroll <= 7:
-                MalePop += 1
-            else:
-                FemalePop += 1
-    
-    Egg0 = Egg0-Egg0+Egg1
-    Egg1 = Egg1-Egg1+Egg2
-    Egg2 = Egg2-Egg2+Egg3
-    Egg3 = Egg3-Egg3+Egg4
-    Egg4 = Egg4-Egg4+Egg5
-    Egg5 = Egg5-Egg5+Egg6
-    Egg6 = Egg6-Egg6+Egg7
-    Egg7 = Egg7-Egg7+Egg8
-    Egg8 = Egg8-Egg8+Egg9
-    Egg9 = Egg9-Egg9+Egg10
-    Egg10 = Egg10-Egg10+Egg11
-    Egg11 = Egg11-Egg11+Egg12
-    Egg12 = Egg12-Egg12+Egg13
-    Egg13 = Egg13-Egg13+Egg14
-    Egg14 = Egg14-Egg14+Egg15    
-    Egg15 = Egg15-Egg15+Egg16
-    Egg16 = Egg16-Egg16+Egg17
-    Egg17 = Egg17-Egg17+Egg18
-    Egg18 = Egg18-Egg18+Egg19
-    Egg19 = Egg19-Egg19+Egg20
-    Egg20 = Egg20-Egg20
+EighthMales = sim(1, 7)
 
 
 
-    if MalePop >=1:
-        for x in range(FemalePop):
-            egg = random.randint(1,100)
-            if egg <= 50:
-                Egg20 += 1
-                
-    elif FemalePop >=1:
-        
-        for x in range(FemalePop):
-            egg = random.randint(1,100)
-            if egg <=10:
-                Egg20 += 1
-
-              
-    
-    print("Total Population:", FemalePop+MalePop)
-    print("Females:", FemalePop)
-    print("Males:", MalePop)
-    print("")
-
-
-    if FemalePop+MalePop >= 20000:
-        print("Population max reached")
-
-        break
-    elif FemalePop == 0 and Egg0 == 0 and Egg1 == 0 and Egg2 == 0 and Egg3 == 0 and Egg4 == 0 and Egg5 == 0 and Egg6 == 0 and Egg7 == 0 and Egg8 == 0 and Egg9 == 0 and Egg10 == 0 and Egg11 == 0 and Egg12 == 0 and Egg13 == 0 and Egg14 == 0 and Egg15 == 0 and Egg16 == 0 and Egg17 == 0 and Egg18 == 0 and Egg19 == 0 and Egg20 == 0:      
-        print("Extinction")
-        break
+plt.plot(x, EighthMales, label = "1:7")
+plt.plot(x, forthMales, label = "2:6")
+plt.plot(x, halfHalf, label = "4:4")
+plt.plot(x, forthFemales, label = "6:2")
+plt.plot(x, eighthFemales, label = "7:1")
+plt.legend()
+plt.show()
+print("done")
+print("no, really")
